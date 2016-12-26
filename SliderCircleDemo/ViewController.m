25c7626f653e99b8c8803d4e13a62f66e009136a
gxl <gxl123@126.com>
2016-12-26 15:14:46 +0800

    init commit

---
+++ SliderCircleDemo/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  SliderCircleDemo
+//
+//  Created by apple on 15/6/30.
+//  Copyright (c) 2015年 HuaZhengInfo. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CircleLHQView.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    CircleLHQView *LHQView = [[CircleLHQView alloc] initWithFrame:CGRectMake(10, 100, 300, 300) andImage:nil];
+    [LHQView addSubViewWithSubView:nil andTitle:@[@"第一个",@"第二个",@"第三个",@"第四个",@"第五个",@"第六个"] andSize:CGSizeMake(60, 60) andcenterImage:nil];
+    [self.view addSubview:LHQView];
+     LHQView.clickSomeOne=^(NSString *str){
+            NSLog(@"%@被点击了",str);
+        };
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
